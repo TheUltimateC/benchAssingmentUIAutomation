efc35665db2d0a6976680714ab7c9779386265b2
cjacquemyn <cedric_jacquemijn@hotmail.com>
2021-09-02 13:20:17 +0200

    safe before laptop hand-in

---
+++ src/test/java/benchAssingmentUIAutomation/StepDefinitions.java
@@ -0,0 +1,32 @@
+package benchAssingmentUIAutomation;
+
+import org.openqa.selenium.OutputType;
+import org.openqa.selenium.TakesScreenshot;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+
+import io.cucumber.java8.En.*;
+public class StepDefinitions {
+
+
+    @Given("today is Sunday")
+    public void today_is_Sunday() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java8.PendingException();
+    }
+
+    @When("I ask whether it's Friday yet")
+    public void i_ask_whether_it_s_Friday_yet() {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java8.PendingException();
+    }
+
+    @Then("I should be told {string}")
+    public void i_should_be_told(String string) {
+        // Write code here that turns the phrase above into concrete actions
+        throw new io.cucumber.java8.PendingException();
+    }
+
+
+}
+
